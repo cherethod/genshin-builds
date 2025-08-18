f16217d8503592da4d7d76c2e9d2ad29fd0178bf
cherethod <sydig.reus@gmail.com>
2025-08-18 17:47:18 +0200

    Fixed call to useEffect

--- src/components/Scrapper.jsx
+++ src/components/Scrapper.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Scrapper = ({setCharacterCompositions}) => {
   const [selectedCorsProxy, setSelectedCorsProxy] = useState(
