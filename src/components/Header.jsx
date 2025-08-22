8d3ba7971294da8229691577476feb59b4204f4e
cherethod <sydig.reus@gmail.com>
2025-08-22 18:04:13 +0200

    Hide button from current status

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,19 @@ const Header = ({ status, setStatus }) => {
         {status === "builds" && "Check Builds"}
       </h1>
       <div>
-        <button onClick={() => setStatus("main")}>Go to Main</button>
-        <button onClick={() => setStatus("owned-characters-selection")}>Set owned Characters</button>
-        <button onClick={() => setStatus("builds")}>Check builds</button>
-        <button onClick={() => setStatus("Scrapper")}>Scrapper</button>
+        {
+          status != "main" && <button onClick={() => setStatus("main")}>Home</button>
+        }
+        {
+          status != "owned-characters-selection" && <button onClick={() => setStatus("owned-characters-selection")}>Set owned Characters</button>
+        
+        }
+        {
+          !status != "builds" && <button onClick={() => setStatus("builds")}>Check Builds</button>
+        }
+        {
+          !status != "Scrapper" && <button onClick={() => setStatus("Scrapper")}>Scrapper</button>
+        }
       </div>
     </header>
   );
